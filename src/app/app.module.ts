5ee2db08f62eb64d093b7b9f6b87ecccfd4a0534
Abu Bakar Siddique <absiddique.live@gmail.com>
2020-01-28 02:36:04 +0600

    [NPM][Modules] Firebase, Fire, ng-bootstrap added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
+import { environment } from "./../environments/environment";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -30,7 +35,11 @@ import { LoginComponent } from "./components/login/login.component";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
