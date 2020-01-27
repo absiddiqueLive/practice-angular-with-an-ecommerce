3ef341a87d4efcffeda4bd4799a2cf50086b3db9
Abu Bakar Siddique <absiddique.live@gmail.com>
2020-01-28 02:29:20 +0600

    [Core][Components] Several components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BsNavbarComponent } from './components/bs-navbar/bs-navbar.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BsNavbarComponent } from "./components/bs-navbar/bs-navbar.component";
+import { HomeComponent } from "./components/home/home.component";
+import { ProductsComponent } from "./components/products/products.component";
+import { ShoppingCartComponent } from "./components/shopping-cart/shopping-cart.component";
+import { CheckOutComponent } from "./components/check-out/check-out.component";
+import { OrderSuccessComponent } from "./components/order-success/order-success.component";
+import { MyOrdersComponent } from "./components/my-orders/my-orders.component";
+import { ProductsComponent as AdminProductsComponent } from "./components/admin/products/products.component";
+import { OrdersComponent as AdminOrdersComponent } from "./components/admin/orders/orders.component";
+import { LoginComponent } from "./components/login/login.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    BsNavbarComponent
+    BsNavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
