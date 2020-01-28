dc9d39bf1a2ec15a9e188effc9d95431bb066742
Abu Bakar Siddique <absiddique.live@gmail.com>
2020-01-28 19:23:09 +0600

    [Core][Components] Auth implemented | Basic

--- src/app/components/bs-navbar/bs-navbar.component.ts
+++ src/app/components/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
+  constructor(public auth: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  logout() {
+    this.auth.logout();
   }
-
 }
