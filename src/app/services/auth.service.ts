4adbc803d0ce64d327f0896f89831a0358396257
Abu Bakar Siddique <absiddique.live@gmail.com>
2020-01-29 02:51:29 +0600

    [Core][Services] Login allow to redirect previously browsed url

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Observable } from 'rxjs';
 import * as firebase from 'firebase';
+import { ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class AuthService {
   // user: firebase.User;
   user$: Observable<firebase.User>; // Updated code
 
-  constructor(private fireAuth: AngularFireAuth) {
+  constructor(private fireAuth: AngularFireAuth, private route: ActivatedRoute) {
     // fireAuth.authState.subscribe((user: firebase.User) => (this.user = user));
     this.user$ = fireAuth.authState; // Updated code
   }
@@ -20,6 +21,10 @@ export class AuthService {
   }
 
   login() {
+    const retuenUrl: string = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+
+    localStorage.setItem('returnUrl', retuenUrl);
+
     this.fireAuth.auth.signInWithRedirect(
       new firebase.auth.GoogleAuthProvider()
     );
