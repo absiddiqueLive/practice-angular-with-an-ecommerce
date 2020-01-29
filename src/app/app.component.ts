7f5aba1d05bd59981c43426f48904bc94e5c63e1
Abu Bakar Siddique <absiddique.live@gmail.com>
2020-01-29 23:52:14 +0600

    [Core][Components][Upgrade] Update user info after login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { AuthService } from './services/auth.service';
 import { Router } from '@angular/router';
+
+import { AuthService } from './services/auth.service';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +10,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private auth: AuthService, private router: Router) {
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private userService: UserService
+  ) {
     auth.user$.subscribe(user => {
       if (user) {
+        this.userService.save(user);
+
         const returnUrl = localStorage.getItem('returnUrl');
         this.router.navigateByUrl(returnUrl);
       }
