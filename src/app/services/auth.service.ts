c42768f1990a853bf2a4b1ae6b2d8bf78879c7e9
Abu Bakar Siddique <absiddique.live@gmail.com>
2020-01-29 23:42:33 +0600

    [Core][Services][Upgrade] Separate auth user by auth user observable

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { Observable } from 'rxjs';
-import * as firebase from 'firebase';
+import { Observable, of } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import * as firebase from 'firebase';
+
+import { AppUser } from '../models/app-user';
+
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +16,11 @@ export class AuthService {
   // user: firebase.User;
   user$: Observable<firebase.User>; // Updated code
 
-  constructor(private fireAuth: AngularFireAuth, private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private fireAuth: AngularFireAuth,
+    private userService: UserService
+  ) {
     // fireAuth.authState.subscribe((user: firebase.User) => (this.user = user));
     this.user$ = fireAuth.authState; // Updated code
   }
@@ -21,12 +30,24 @@ export class AuthService {
   }
 
   login() {
-    const retuenUrl: string = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+    const retuenUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
 
     localStorage.setItem('returnUrl', retuenUrl);
 
     this.fireAuth.auth.signInWithRedirect(
       new firebase.auth.GoogleAuthProvider()
+    );
+  }
+
+  // Get app user observable
+  get appUser$(): Observable<AppUser> {
+    return this.user$.pipe(
+      switchMap(user => {
+        if (user) {
+          return this.userService.get(user.uid).valueChanges();
+        }
+        return of(null);
+      })
     );
   }
 }
