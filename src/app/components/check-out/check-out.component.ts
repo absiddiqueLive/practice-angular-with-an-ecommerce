1eaba7307d72f512a5313c8f37b52a5630eeef1d
Abu Bakar Siddique <absiddique.live@gmail.com>
2020-02-22 01:17:07 +0600

    [Core][Components] Check out used seperate shopping form component

--- src/app/components/check-out/check-out.component.ts
+++ src/app/components/check-out/check-out.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
-import { Order } from 'src/app/models/order';
 import { ShoppingCart } from 'src/app/models/shopping-cart';
 
-import { AuthService } from 'src/app/services/auth.service';
-import { OrderService } from 'src/app/services/order.service';
 import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
 
 @Component({
@@ -14,36 +10,12 @@ import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit, OnDestroy {
-  shipping: any = {};
-  cart: ShoppingCart;
-  userId: string;
-  cartSubscription: Subscription;
-  authSubscription: Subscription;
+export class CheckOutComponent implements OnInit {
+  cart$: Observable<ShoppingCart>;
 
-  constructor(
-    private auth: AuthService,
-    private cartService: ShoppingCartService,
-    private orderService: OrderService,
-    private route: Router
-  ) {}
-
-  async placeOrder() {
-    const order = new Order(this.userId, this.shipping, this.cart);
-    const result = await this.orderService.placedOrder(order);
-    this.route.navigate(['/order-success', result.key]);
-  }
+  constructor(private cartService: ShoppingCartService) {}
 
   async ngOnInit() {
-    const cart$ = await this.cartService.get();
-    this.cartSubscription = cart$.subscribe(cart => (this.cart = cart));
-    this.authSubscription = this.auth.user$.subscribe(
-      user => (this.userId = user.uid)
-    );
-  }
-
-  ngOnDestroy() {
-    this.cartSubscription.unsubscribe();
-    this.authSubscription.unsubscribe();
+    this.cart$ = await this.cartService.get();
   }
 }
